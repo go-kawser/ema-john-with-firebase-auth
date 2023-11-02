884dd01e70b1e5cf332b2c78b17f3de568b47886
Kawser420 <muhammadkawser202@gmail.com>
2023-11-03 00:27:22 +0600

    firebase authentication login and logOut created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+
+
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(result => { })
+            .catch(error => console.log(error.message))
+    }
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -13,6 +24,7 @@ const Header = () => {
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/sign-up">Sign up</Link>
+                {user && <span>{user.email} <button onClick={handleLogOut}>Sign Out</button></span>}
             </div>
         </nav>
     );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Login = () => {
+
+    const [error, setError] = useState(null);
+    const { signIn } = useContext(AuthContext);
+
+
+    const handleLogin = e => {
+        e.preventDefault();
+        setError('');
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+            })
+
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="" required />
@@ -16,6 +44,8 @@ const Login = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p className='text-error'>{error}</p>
+            <p><small>New To Ema-John? <Link to="/sign-up">Create New Account</Link></small></p>
         </div>
     );
 };
